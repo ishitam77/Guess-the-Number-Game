b03c13e8489971333c9a9450820914a06bc876b9
Ishita Maurya <86301182+ishitam77@users.noreply.github.com>
2022-02-14 19:25:18 +0530

    Update Game1.c

--- Game1.c
+++ Game1.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+#include<conio.h>
 int main(){
     int cho;
     int numb,guessnumb; 
     do{
-        
-        printf("-----MENU-----\n");
-        printf("1. Let's Play\n");
-        printf("2. Exit\n");
-        printf("Enter Your Choice Here :");
+        system("COLOR 02");
+        printf("\t\t\t-----MENU-----\n");
+        printf("\t\t\t1. Let's Play\n");
+        printf("\t\t\t2. Exit\n");
+        printf("\t\t\tEnter Your Choice Here :");
         scanf("%d",&cho);
          int countguess=1;
         switch(cho){
@@ -19,34 +20,34 @@ int main(){
             srand(time(0));
             numb=rand()%100;
             do{
-                printf("----Guess the Number Between 0 to 100----\n");
+                printf("\t\t\t----Guess the Number Between 0 to 100----\n");
+                printf("\t\t\t");
                 scanf("%d",&guessnumb);
                 if (guessnumb<numb){
-                    printf("Your Guessed Number is Smaller\n");
-                    printf("Enter Higher Number Plzz\n");
+                    printf("\t\t\tYour Guessed Number is Smaller\n");
+                    printf("\t\t\tEnter Higher Number Plzz\n");
                 }
                  else if (guessnumb>numb){
-                    printf("Your Guessed Number is Greater\n");
-                    printf("Enter Lower Number Plzz\n");
+                    printf("\t\t\tYour Guessed Number is Greater\n");
+                    printf("\t\t\tEnter Lower Number Plzz\n");
                 }
                 else{
-                    printf("Congo!!!That's Right\n");
-                    printf("You Guessed the Correct Number in %d attempts\n",countguess);
+                    printf("\t\t\tCongo!!!That's Right\n");
+                    printf("\t\t\tYou Guessed the Correct Number in %d attempts\n",countguess);
                 }
                 countguess++;
             }while(guessnumb!=numb);
             break;
 
             case 2:
-
-            printf("****Thanks For Visiting****\n");
+            printf("\t\t\t****Thanks For Visiting****\n");
             break;
 
             default:
-            
-            printf("Wrong Choice_Plzz Enter Again\n");
+            printf("\t\t\tWrong Choice_Plzz Enter Again\n");
             break;
         }
     }while(cho!=2);
     return 0;
+    getch();
 }
